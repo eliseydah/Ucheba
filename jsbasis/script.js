4b9feeaa974bd729130846cbceb61e8ac48e0fbf
Daria Eliseeva <elisey422das@gmail.com>
2023-05-01 20:11:40 +0200

    triangle

--- jsbasis/script.js
+++ jsbasis/script.js
@@ -1,4 +1,11 @@
+//task 1
 var a = 10
 var b = 5
 console.log(a);
 console.log(b);
+//task 2 
+function calcTriangleSquare(a, b, sin) {
+    return a * b * sin * 0.5
+
+}
+console.log(calcTriangleSquare(5, 10, 1))
